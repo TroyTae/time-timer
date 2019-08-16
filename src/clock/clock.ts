1d547c8b9cc2831bb4e68ed3b3edef7172301ef9
Troy Tae <tjy970721@gmail.com>
2019-08-16 20:33:53 +0900

    Redesign

--- src/clock/clock.ts
+++ src/clock/clock.ts
@@ -3,8 +3,8 @@ import './clock.scss';
 const clock = document.getElementById('clock');
 
 if (clock) {
+  // Minute Display
   const temp = document.createElement('div');
-
   Array(12).fill(null).forEach((v, i) => {
     const min = i * 5;
     // Insert minute dot
@@ -12,11 +12,48 @@ if (clock) {
     const dot = temp.children[0];
     clock.appendChild(dot);
     // Insert minute hand
-    temp.innerHTML = `<button class="min m${min}">${min}</button>`;
+    temp.innerHTML = `<span class="min m${min}">${min}</span>`;
     const hand = temp.children[0];
     hand.addEventListener('click', () => {
       console.log('Click Minute Hand');
     });
     clock.appendChild(hand);
   });
+
+  // Canvas Fill Control
+  const circle = clock.querySelector('.circle');
+
+  if (circle) {
+    const canvas = document.createElement('canvas');
+    const resize = () => {
+      canvas.width = circle.clientWidth;
+      canvas.height = circle.clientHeight;
+    };
+    resize();
+
+    const context = canvas.getContext('2d');
+    if (context) {
+      const draw = () => {
+        context.fillStyle = '#E31936';
+        context.beginPath();
+        context.arc(
+            canvas.width / 2,
+            canvas.height / 2,
+            canvas.width / 2,
+            Math.PI / 2 * 3,
+            Math.PI / 3 * 2,
+            true
+        );
+        context.lineTo(canvas.width / 2, canvas.height / 2);
+        context.closePath();
+        context.fill();
+      };
+      window.addEventListener('resize', () => {
+        resize();
+        draw();
+      });
+      circle.appendChild(canvas);
+      draw();
+    }
+  }
 }
--- src/timer/timer.ts
+++ src/timer/timer.ts
@@ -13,20 +13,10 @@ if (timer) {
   });
   timer.appendChild(reset);
 
-  // Minute Text
-  temp.innerHTML = '<span class="time min">15</span>';
-  const min = temp.children[0];
-  timer.appendChild(min);
-
-  // Colon Text
-  temp.innerHTML = '<span class="time colon">:</span>';
-  const colon = temp.children[0];
-  timer.appendChild(colon);
-
-  // Sec Text
-  temp.innerHTML = '<span class="time sec">45</span>';
-  const sec = temp.children[0];
-  timer.appendChild(sec);
+  // Time Text
+  temp.innerHTML = '<span class="time">15:25</span>';
+  const time = temp.children[0];
+  timer.appendChild(time);
 
   // Reset Button
   temp.innerHTML = '<button class="btn start"></button>';
