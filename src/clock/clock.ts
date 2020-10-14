f4aeaf5ed9cca219805eab27bf4f5879e630bf82
태재영 <tjy970721@gmail.com>
2020-10-14 16:53:33 +0900

    refactor: noliter clock

--- src/clock/clock.ts
+++ src/clock/clock.ts
@@ -1,17 +1,33 @@
-import * as styles from './clock.scss';
 import spaces from 'one-spaces';
+import {
+  $,
+  ATTR_CLASS,
+  TAG_NAME_DIV,
+  TAG_NAME_SPAN,
+} from 'noliter';
+import * as styles from './clock.scss';
 
 const clock = document.getElementById('clock');
 
 if (clock) {
-  Array(12).fill(null).forEach((v, i) => {
+  [...Array(12)].forEach((v, i) => {
     const min = i * 5;
-    const dot = document.createElement('div');
-    dot.className = spaces(styles.dot, styles[`m${min}`]);
-    const num = document.createElement('span');
-    num.className = spaces(styles.min, styles[`m${min}`]);
-    num.textContent = min.toString();
-    clock.appendChild(dot);
-    clock.appendChild(num);
+    clock.appendChild(
+      $(TAG_NAME_DIV)
+        .attrs(ATTR_CLASS, spaces(
+          styles.dot,
+          styles[`m${min}`],
+        ))
+        .dom
+    );
+    clock.appendChild(
+      $(TAG_NAME_SPAN)
+        .attrs(ATTR_CLASS, spaces(
+          styles.min,
+          styles[`m${min}`],
+        ))
+        .append(min.toString())
+        .dom
+    );
   });
 }
