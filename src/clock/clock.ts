fd38044bf70031851b0e1057b9f1d535d6555171
태재영 <tjy970721@gmail.com>
2020-10-15 19:09:06 +0900

    reafactor: export deafult dom

--- src/clock/clock.ts
+++ src/clock/clock.ts
@@ -9,29 +9,27 @@ import {
 } from 'noliter';
 import * as styles from './clock.scss';
 
-export default function Clock() {
-  return $(TAG_NAME_SECTION)
-    .attrs(ATTR_ID, styles.clock)
-    .append(
-      $(TAG_NAME_DIV).attrs(ATTR_ID, styles.circle),
-      ...[...Array(12)].reduce((arr, v, i) => {
-        const min = i * 5;
-        arr.push(
-          $(TAG_NAME_DIV)
-            .attrs(ATTR_CLASS, spaces(
-              styles.dot,
-              styles[`m${min}`],
-            ))
-        );
-        arr.push(
-          $(TAG_NAME_SPAN)
-            .attrs(ATTR_CLASS, spaces(
-              styles.min,
-              styles[`m${min}`],
-            ))
-            .append(`${min}`)
-        );
-        return arr;
-      }, []),
-    );
-}
+export default $(TAG_NAME_SECTION)
+  .attrs(ATTR_ID, styles.clock)
+  .append(
+    $(TAG_NAME_DIV).attrs(ATTR_ID, styles.circle),
+    ...[...Array(12)].reduce((arr, v, i) => {
+      const min = i * 5;
+      arr.push(
+        $(TAG_NAME_DIV)
+          .attrs(ATTR_CLASS, spaces(
+            styles.dot,
+            styles[`m${min}`],
+          ))
+      );
+      arr.push(
+        $(TAG_NAME_SPAN)
+          .attrs(ATTR_CLASS, spaces(
+            styles.min,
+            styles[`m${min}`],
+          ))
+          .append(`${min}`)
+      );
+      return arr;
+    }, []),
+  );
