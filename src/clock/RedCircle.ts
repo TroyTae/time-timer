d8a82ea672c372d17cf5f6abdbfb80c5eae62854
태재영 <tjy970721@gmail.com>
2020-10-30 17:02:45 +0900

    feat: support mobile

--- src/clock/RedCircle.ts
+++ src/clock/RedCircle.ts
@@ -2,11 +2,14 @@ import {
   $,
   ATTR_ID,
   ATTR_HREF,
+  TAG_NAME_CANVAS,
   EVENT_TYPE_MOUSEDOWN,
   EVENT_TYPE_MOUSELEAVE,
   EVENT_TYPE_MOUSEMOVE,
   EVENT_TYPE_MOUSEUP,
-  TAG_NAME_CANVAS,
+  EVENT_TYPE_TOUCHSTART,
+  EVENT_TYPE_TOUCHEND,
+  EVENT_TYPE_TOUCHMOVE,
 } from 'noliter';
 import * as styles from './RedCircle.scss';
 import {
@@ -37,25 +40,42 @@ function getTanDegree(x: number, y: number) {
   return Math.atan(y / x) * 180 / Math.PI;
 }
 
+function drawWithCoordinate(x: number, y: number) {
+  const degree = getTanDegree(x, y);
+  if ((x < 0 && 0 <= y) || (x < 0 && y < 0)) {
+    setDegree(degree + 270);
+  } else {
+    setDegree(degree + 90);
+  }
+  syncTimeText();
+  draw();
+}
+
 const RedCircle = $(TAG_NAME_CANVAS)
   .sa(ATTR_ID, styles.canvas)
   .on(EVENT_TYPE_MOUSEMOVE, function (e) {
     if (isModified() && !isStarted()) {
-      const x = e.offsetX - canvas.width / 2;
-      const y = e.offsetY - canvas.height / 2;
-      const degree = getTanDegree(x, y);
-      if ((x < 0 && 0 <= y) || (x < 0 && y < 0)) {
-        setDegree(degree + 270);
-      } else {
-        setDegree(degree + 90);
-      }
-      syncTimeText();
-      draw();
+      drawWithCoordinate(
+        e.offsetX - canvas.width / 2,
+        e.offsetY - canvas.height / 2,
+      );
+    }
+  }, { passive: true })
+  .on(EVENT_TYPE_TOUCHMOVE, function (e) {
+    if (isModified() && !isStarted()) {
+      const touch = e.touches[0];
+      const rect = canvas.getBoundingClientRect();
+      drawWithCoordinate(
+        touch.pageX - rect.x - canvas.width / 2,
+        touch.pageY - rect.y - canvas.height / 2,
+      );
     }
   }, { passive: true })
   .on(EVENT_TYPE_MOUSEDOWN, startHandling)
+  .on(EVENT_TYPE_TOUCHSTART, startHandling)
   .on(EVENT_TYPE_MOUSEUP, stopHandling)
-  .on(EVENT_TYPE_MOUSELEAVE, stopHandling);
+  .on(EVENT_TYPE_MOUSELEAVE, stopHandling)
+  .on(EVENT_TYPE_TOUCHEND, stopHandling);
 
 const canvas = RedCircle.dom;
 const context = canvas.getContext('2d');
